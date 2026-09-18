8d0075eb052858bb3781c740bf3d6f93e066d9e6
vithorcarmo08-prog <vithorcarmo08@gmail.com>
2026-09-18 16:39:24 -0300

    Update and rename e.g.,exercicio23.php to exercicio23.php

---
+++ exercicio23.php
@@ -0,0 +1,28 @@
+<?php
+//******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: Flores
+Turma: ESOFT-2
+Componentes: 
+             26008950-2 -  Vinicius de Medeiros
+             26009345-2 -  Vithor do Carmo Ariza
+             26011979-2 -  Rafael Luis do P.B Kluck
+             26010098-2 -  Rafael José
+             26008802-2 -  João Victor Marchezan
+             26013906-2 -  Lucas de Oliveira
+Data: 18 de Setembro de 2026
+
+$Base = 10.0;
+$Altura = 60.0;
+$Area = ($Base * $Altura) / 2;
+
+if ($Area > 50) {
+    echo "A área do triângulo é maior que 50.";
+} else {
+    echo "A área do triângulo é menor que 50.";
+}
+// O enunciado pede que calcule e exiba a área de um triângulo. O código realiza exatamente isso; ele calcula e mostra o resultado que se pede.
+?>
+
+
