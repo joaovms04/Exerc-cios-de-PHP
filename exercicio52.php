b81f72d9b3f485ac5054c2e62620b4a8498b033c
vithorcarmo08-prog <vithorcarmo08@gmail.com>
2026-09-18 16:41:15 -0300

    Update and rename e.g.,exercicio52.php to exercicio52.php

---
+++ exercicio52.php
@@ -0,0 +1,23 @@
+<?php
+//******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: Flores
+Turma: ESOFT-2
+Componentes: 
+             26008950-2 -  Vinicius de Medeiros
+             26009345-2 -  Vithor do Carmo Ariza
+             26011979-2 -  Rafael Luis do P.B Kluck
+             26010098-2 -  Rafael José
+             26008802-2 -  João Victor Marchezan
+             26013906-2 -  Lucas de Oliveira
+Data: 18 de Setembro de 2026
+
+$reais = (float) readline("Digite o valor em reais: ");
+$cotacao = (float) readline("Digite a cotação do dólar: ");
+
+$dolares = $reais / $cotacao;
+
+echo "Valor em dólares: US$ " . number_format($dolares, 2, ',', '.');
+// Este código pega um valor em REAL e faz adivisão pelo preço do dolar.
+?>
