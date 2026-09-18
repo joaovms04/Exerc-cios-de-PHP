48edf233435c41fd1d10a072a713e2eb64908071
vithorcarmo08-prog <vithorcarmo08@gmail.com>
2026-09-18 16:39:04 -0300

    Update and rename e.g.,exercicio17.php to exercicio17.php

--- exercicio17.php
+++ exercicio17.php
@@ -1,4 +1,17 @@
 <?php
+//******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: Flores
+Turma: ESOFT-2
+Componentes: 
+             26008950-2 -  Vinicius de Medeiros
+             26009345-2 -  Vithor do Carmo Ariza
+             26011979-2 -  Rafael Luis do P.B Kluck
+             26010098-2 -  Rafael José
+             26008802-2 -  João Victor Marchezan
+             26013906-2 -  Lucas de Oliveira
+Data: 18 de Setembro de 2026
 
 $dividendo = (int) readline("Digite o dividendo: ");
 $divisor = (int) readline("Digite o divisor: ");
