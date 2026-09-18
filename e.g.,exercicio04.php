c1031c20a22f554f050d4f0a2aaa9b634cfa44d0
vithorcarmo08-prog <vithorcarmo08@gmail.com>
2026-09-18 16:37:43 -0300

    Update and rename e.g.,exercicio04.php to exercicio04.php

--- e.g.,exercicio04.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$numero = (int) readline("Digite um número inteiro: ");
-
-echo "Número lido = " . $numero;
-// Esse código vai ler o número que você digitar e apresentar uma mensagem dizendo que o número foi lido com o número em si.
-?>
---
+++ exercicio04.php
@@ -0,0 +1,19 @@
+<?php
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: Flores
+Turma: ESOFT-2
+Componentes: 
+             26008950-2 -  Vinicius de Medeiros
+             26009345-2 -  Vithor do Carmo Ariza
+             26011979-2 -  Rafael Luis do P.B Kluck
+             26010098-2 -  Rafael José
+             26008802-2 -  João Victor Marchezan
+             26013906-2 -  Lucas de Oliveira
+Data: 18 de Setembro de 2026 
+$numero = (int) readline("Digite um número inteiro: ");
+
+echo "Número lido = " . $numero;
+// Esse código vai ler o número que você digitar e apresentar uma mensagem dizendo que o número foi lido com o número em si.
+?>
